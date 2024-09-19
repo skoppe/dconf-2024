b25d9a3c25c8939e068ad6e68309253e76556d4c
skoppe <mail@skoppe.eu>
2024-09-19 08:55:53 +0000

    Deploying to gh-pages from @ skoppe/dconf-2024@d1ca519b919e01bebb1ea1c39d5ac9bfd6ce8210 🚀

---
+++ assets/slidev/default-B3bxM23W.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-CBXeOvZU.js";import{_ as r}from"../index-C0UWz8-6.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
